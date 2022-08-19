092b826f04f5d3f81c5082d500b27eb1a697e9d8
César Okada <cesar.okada@fatec.sp.gov.br>
2022-08-19 18:09:46 -0300

    Apostila 04 bloco 2.7.2 concluído - Sem os erros

--- src/index.js
+++ src/index.js
@@ -2,27 +2,60 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free'
-import './Pedido'
+import Pedido from './Pedido'
 
 const App = () => {
-         return(
-                           <div className='container border rounded mt-2 p-2'>
-                           <div className='row border-bottom m-2'>
-                                    <h1 className='display-5 text-center'>Seus pedidos</h1>
-                           </div>
-
-                           {/*Linha para primeiro pedido*/}
-                           <div className="row">
-                                    <div className="col-sm-8 col-md-6 m-2">
-                                    {/*Aqui entra o primeiro pedido*/}
-                                    </div>
-                           </div>
-
-                           </div>
-         )               
-}
-
-ReactDOM.render(
+         return (
+           // componente principal
+           <div class='container border rounded mt-2'>
+             <div className='row border-bottom m-2'>
+               <h1 className='display-5 text-center'>Seus pedidos</h1>
+             </div>
+             {/* linha para o primeiro pedido */}
+             <div className="row">
+               <div className="col-sm-8 col-md-6 m-2">
+                 {/* aqui vai entrar o primeiro pedido */}
+                 <Pedido 
+                   data="22/04/2021" 
+                   icone="fa-solid fa-hdd fa-2x" 
+                   titulo="SSD" 
+                   descricao="SSD Kingston A400 - SATA"/>
+               </div>
+             </div>
+       
+             {/* linha para o segundo pedido */}
+             <div className="row">
+               <div className="col-sm-8 col-md-6 m-2">
+                 {/* aqui vai ficar o segundo pedido */}
+                 <Pedido 
+                   data="20/04/2021"
+                   icone="fa-solid fa-book fa-2x"
+                   titulo="Livro" 
+                   descricao="Concrete Mathematics - Donald Knuth"
+                 />
+               </div>
+             </div>
+       
+             {/* linha para o terceiro pedido */}
+             <div className="row">
+               <div className="col-sm-8 col-md-6 m-2">
+                 {/* aqui vai ficar o terceiro pedido */}
+                 <Pedido 
+                   data="21/01/2021"
+                   icone="fa-solid fa-laptop fa-2x"
+                   titulo="Notebook" 
+                   descricao="Notebook Dell - 8Gb - i5"
+                 />
+               </div>
+             </div>
+       
+           </div>
+           
+         )
+       }
+       
+       
+       ReactDOM.render(
          <App />,
          document.querySelector('#root')
-         )
+       )
