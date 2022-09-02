840625141d42acd8fa87f9cf13494b2a36aa9539
Pablo Cardoso <pablo.silva23@fatec.sp.gov.br>
2022-09-02 16:32:32 -0300

    Fim da apostila 4

--- src/index.js
+++ src/index.js
@@ -1,61 +1,85 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
+import Pedido from './Pedido'
+import Cartao from './Cartao'
+import Feedback from './Feedback'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import Pedido from './Pedido'
-
 const App = () => {
-         return (
-           // componente principal
-           <div class='container border rounded mt-2'>
-             <div className='row border-bottom m-2'>
-               <h1 className='display-5 text-center'>Seus pedidos</h1>
-             </div>
-             {/* linha para o primeiro pedido */}
-             <div className="row">
-               <div className="col-sm-8 col-md-6 m-2">
-                 {/* aqui vai entrar o primeiro pedido */}
-                 <Pedido 
-                   data="22/04/2021" 
-                   icone="fa-solid fa-hdd fa-2x" 
-                   titulo="SSD" 
-                   descricao="SSD Kingston A400 - SATA"/>
-               </div>
-             </div>
-       
-             {/* linha para o segundo pedido */}
-             <div className="row">
-               <div className="col-sm-8 col-md-6 m-2">
-                 {/* aqui vai ficar o segundo pedido */}
-                 <Pedido 
-                   data="20/04/2021"
-                   icone="fa-solid fa-book fa-2x"
-                   titulo="Livro" 
-                   descricao="Concrete Mathematics - Donald Knuth"
-                 />
-               </div>
-             </div>
-       
-             {/* linha para o terceiro pedido */}
-             <div className="row">
-               <div className="col-sm-8 col-md-6 m-2">
-                 {/* aqui vai ficar o terceiro pedido */}
-                 <Pedido 
-                   data="21/01/2021"
-                   icone="fa-solid fa-laptop fa-2x"
-                   titulo="Notebook" 
-                   descricao="Notebook Dell - 8Gb - i5"
-                 />
-               </div>
-             </div>
-       
-           </div>
-           
-         )
-       }
-       
-       
-       ReactDOM.render(
-         <App />,
-         document.querySelector('#root')
-       )
+  const textoOK = 'Já chegou'
+  const textoNOK = 'Ainda não chegou'
+  const funcaoOK = () => alert ('Agradecemos a sua confirmação')
+  const funcaoNOK = () => alert ('Verificaremos o ocorrido')
+  const componenteFeedback = (
+  <Feedback
+    textoOK={textoOK}
+    textoNOK={textoNOK}
+    funcaoOK={funcaoOK}
+    funcaoNOK={funcaoNOK}
+  />
+  )
+
+  return (
+    // componente principal
+    <div class='container border rounded mt-2'>
+      <div className='row border-bottom m-2'>
+        <h1 className='display-5 text-center'>Seus pedidos</h1>
+      </div>
+      {/* linha para o primeiro pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai entrar o primeiro pedido */}
+          <Cartao
+            cabecalho="22/04/2021">
+            <Pedido 
+              icone="fa-solid fa-hdd fa-2x" 
+              titulo="SSD" 
+              descricao="SSD Kingston A400 - SATA"/>
+              {componenteFeedback}
+          </Cartao>
+        </div>
+      </div>
+
+      {/* linha para o segundo pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai ficar o segundo pedido */}
+          <Cartao
+            cabecalho="22/04/2022">
+            <Pedido 
+              icone="fa-solid fa-book fa-2x"
+              titulo="Livro" 
+              descricao="Concrete Mathematics - Donald Knuth"
+            />
+            {componenteFeedback}
+          </Cartao>
+        </div>
+      </div>
+
+      {/* linha para o terceiro pedido */}
+      <div className="row">
+        <div className="col-sm-8 col-md-6 m-2">
+          {/* aqui vai ficar o terceiro pedido */}
+          {/* Cartao[cabecalho=22/05/2022] */}
+          
+          <Cartao cabecalho="22/05/2022">
+            <Pedido 
+              icone="fa-solid fa-laptop fa-2x"
+              titulo="Notebook" 
+              descricao="Notebook Dell - 8Gb - i5"
+            />
+          {componenteFeedback}
+          </Cartao>
+        </div>
+      </div>
+
+    </div>
+    
+  )
+}
+
+
+ReactDOM.render(
+  <App />,
+  document.querySelector('#root')
+)
